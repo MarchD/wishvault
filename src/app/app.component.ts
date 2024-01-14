099ba78057f2eedd014039c8095653e4c1d18acc
Maryna Dubchak <dubchmar@gmail.com>
2024-01-14 20:14:09 +0200

    add header and navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'wishvault';
+  navigationList: { label: string, path: string }[] = [
+    {
+      label: 'Profile',
+      path: '/profile'
+    }
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/icons/logo/logo.component';
+import { ListsComponent } from './pages/lists/lists.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoComponent,
+    ListsComponent,
+    ProfileComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
